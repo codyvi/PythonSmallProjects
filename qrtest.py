ee27f07a6d0375a9fd9612f0afd25af3a94bc68e
codyvi <davidcantumartinez@gmail.com>
2019-08-30 14:52:18 -0500

    Made the text in qr more legible

--- qrtest.py
+++ qrtest.py
@@ -1,6 +1,8 @@
 import qrcode 
 
-data = input("¿Cual es tu nombre? \n ")
+data = "Nombre: "
+data += input("¿Cual es tu nombre? \n ")
+data += " Matrícula: "
 data += input("Cuál es tu matricula? \n ")
 qr = qrcode.make(data)
 qr.save("myQR.jpg")
