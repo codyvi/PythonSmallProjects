f6d5c2c682b168e44b3660cdf9474d8013c6ed8c
codyvi <davidcantumartinez@gmail.com>
2019-08-29 21:40:38 -0500

    Done change to the mail and pass

--- mailtest.py
+++ mailtest.py
@@ -2,8 +2,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-gmailaddress = "noreplyqrcodes@gmail.com"
-gmailpassword = "8Y&(d2bu"
+gmailaddress = ""
+gmailpassword = ""
 mailto = input("what email address do you want to send your message to? \n ")
 msg = MIMEMultipart()
 msg['From'] = gmailaddress
