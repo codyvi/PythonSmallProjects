6a920c35d15c91402b1816318418e3c222e43aa2
codyvi <davidcantumartinez@gmail.com>
2019-09-07 18:04:15 -0500

    Fixed Typos

--- Carbono en Arboles.py
+++ Carbono en Arboles.py
@@ -1,20 +1,20 @@
 import math
 
 DensidadArbol = float(input('Dame la densidad del arbol en metros: '))
-AlturaAbrol = float(input('Dame la altura del arbol en metros: '))
+AlturaArbol = float(input('Dame la altura del arbol en metros: '))
 DiametroArbol = float(input('Dame el diametro del arbol en metros: '))
 
 
 g = math.pi/4 * math.pow(DiametroArbol, 2)
 
-V = g * AlturaAbrol * .6
+V = g * AlturaArbol * .6
 
 B = V * DensidadArbol * 1.35
 
 C = B *.49
 
-print ('G = ', +g)
-print ('V = ', +V)
-print ('B = ', +B)
-print ('El Carbono en el arbol es ', +C, ' kg')
+print ('G =', +g)
+print ('V =', +V)
+print ('B =', +B)
+print ('El Carbono en el arbol es', +C, 'kg')
 
