44fd2b52430308bfaa7366ef4c9972e2a0b0404c
codyvi <davidcantumartinez@gmail.com>
2019-09-07 17:58:03 -0500

    Script para calcular carbono en arboles

---
+++ Carbono en Arboles.py
@@ -0,0 +1,20 @@
+import math
+
+DensidadArbol = float(input('Dame la densidad del arbol en metros: '))
+AlturaAbrol = float(input('Dame la altura del arbol en metros: '))
+DiametroArbol = float(input('Dame el diametro del arbol en metros: '))
+
+
+g = math.pi/4 * math.pow(DiametroArbol, 2)
+
+V = g * AlturaAbrol * .6
+
+B = V * DensidadArbol * 1.35
+
+C = B *.49
+
+print ('G = ', +g)
+print ('V = ', +V)
+print ('B = ', +B)
+print ('El Carbono en el arbol es ', +C, ' kg')
+
