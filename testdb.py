d50a2a02f43a5fa314ea201fe51846f4ce833bdc
codyvi <davidcantumartinez@gmail.com>
2019-10-10 17:55:51 -0500

    ¿Alguien me va querer algún día?

--- testdb.py
+++ testdb.py
@@ -3,3 +3,4 @@ db = TinyDB('db.json')
 db.insert({'id': 1, 'name': 'car1', 'color': 'green'})
 db.insert({'id': 2, 'name': 'car2', 'color': 'red'})
 db.insert({'id': 3, 'name': 'car3', 'color': 'blue'})
+
--- testdb2.py
+++ testdb2.py
@@ -1,9 +1,9 @@
 from tinydb import TinyDB, Query
+
+
+
 db = TinyDB('db.json')
 Cars = Query()
 yeet = db.search(Cars.color == 'green')
 
-
-
 print(yeet)
-
