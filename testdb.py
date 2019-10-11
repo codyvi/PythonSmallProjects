4a8a6b03f8ddb62da153e416432397ff388243b0
codyvi <davidcantumartinez@gmail.com>
2019-10-11 16:31:25 -0500

    Done

--- testdb.py
+++ testdb.py
@@ -1,6 +1,6 @@
 from tinydb import TinyDB, Query
 db = TinyDB('db.json')
-db.insert({'id': 1, 'name': 'car1', 'color': 'green'})
-db.insert({'id': 2, 'name': 'car2', 'color': 'red'})
-db.insert({'id': 3, 'name': 'car3', 'color': 'blue'})
+db.insert({'id': 1, 'name': 'david', 'dinero': '8000'})
+db.insert({'id': 2, 'name': 'carlos', 'dinero': '5000'})
+db.insert({'id': 3, 'name': 'turriza', 'dinero': '0'})
 
--- testdb2.py
+++ testdb2.py
@@ -1,5 +1,4 @@
 from tinydb import TinyDB, Query
-import json
 
 def Convert(string): 
     li = list(string.split(" ")) 
@@ -7,19 +6,20 @@ def Convert(string):
 
 
 db = TinyDB('db.json')
-Cars = Query()
-yeet = db.search(Cars.color == 'green')
+Name = Query()
+yeet = db.search(Name.name == 'david')
 
 
     
 #Convierto a string la lista con un atributo
 str1 = ''.join(str(e) for e in yeet)
-print(str1)
+# print(str1)
 #conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
 newli = Convert(str1)
 
 s = newli[len(newli) - 1]
 #le quito los '{' del json
 s = s.translate({ord(i): None for i in '"}'})
+s = s.translate({ord(i): None for i in "'"})
 
 print(s)
