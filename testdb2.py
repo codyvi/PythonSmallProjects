3efe301b493ac9af0969c4dfb2817eb73bc07f5d
codyvi <davidcantumartinez@gmail.com>
2019-10-11 15:41:12 -0500

    Hice cambios y ahora no jala, deje lo que si jala, quiero optimizar

--- testdb2.py
+++ testdb2.py
@@ -10,15 +10,20 @@ db = TinyDB('db.json')
 Cars = Query()
 yeet = db.search(Cars.color == 'green')
 
-print(yeet)
+for object in yeet:
+   print(object.id)
+   print(object.name)
+   print(object.color)
+
+    
 #Convierto a string la lista con un atributo
-str1 = ''.join(str(e) for e in yeet)
-print(str1)
-#conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
-newli = Convert(str1)
+# str1 = ''.join(str(e) for e in yeet)
+# print(str1)
+# #conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
+# newli = Convert(str1)
 
-s = newli[len(newli) - 1]
-#le quito los '{' del json
-s = s.translate({ord(i): None for i in '"}'})
+# s = newli[len(newli) - 1]
+# #le quito los '{' del json
+# s = s.translate({ord(i): None for i in '"}'})
 
-print(s)
+# print(s)
