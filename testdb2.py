179b5be8cc214ae3105bc976b9314f2f692b6037
codyvi <davidcantumartinez@gmail.com>
2019-10-11 15:04:29 -0500

    Comentado y todo papa

--- testdb2.py
+++ testdb2.py
@@ -11,14 +11,14 @@ Cars = Query()
 yeet = db.search(Cars.color == 'green')
 
 print(yeet)
-
+#Convierto a string la lista con un atributo
 str1 = ''.join(str(e) for e in yeet)
 print(str1)
-
+#conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
 newli = Convert(str1)
 
 s = newli[len(newli) - 1]
-
+#le quito los '{' del json
 s = s.translate({ord(i): None for i in '"}'})
 
 print(s)
